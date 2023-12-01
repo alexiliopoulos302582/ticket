723ec4c2b39db695a57f51060de90a6bafe0734f
alexiliopoulos302582 <96843774+alexiliopoulos302582@users.noreply.github.com>
2023-12-01 16:23:28 +0200

    Add files via upload

---
+++ com.tickets/src/main/java/com/tickets/entity/User.java
@@ -0,0 +1,107 @@
+package com.tickets.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Collection;
+
+@Entity
+@AllArgsConstructor
+@Getter
+@Setter
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String fullname;
+    private String phonenumber;
+    private String email;
+
+    private String enabled;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(
+    name = "user_id", referencedColumnName = "id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")    )
+    private Collection<Role> role;
+
+    public User() {
+        super();
+    }
+
+    public User(String username, String password,
+                String fullname, String phonenumber, String email, String enabled, Collection<Role> role) {
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+        this.phonenumber = phonenumber;
+        this.email = email;
+        this.enabled = enabled;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(String enabled) {
+        this.enabled = enabled;
+    }
+
+    public Collection<Role> getRole() {
+        return role;
+    }
+
+    public void setRole(Collection<Role> role) {
+        this.role = role;
+    }
+}
